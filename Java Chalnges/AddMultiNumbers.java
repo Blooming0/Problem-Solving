721a0672c4b624875e398a6a0470f64776adb775
Blooming0 <154713499+Blooming0@users.noreply.github.com>
2024-11-02 13:04:11 +0300

    Rename create new File Called js Chalnges

--- Java Chalnges/AddMultiNumbers.java
+++ Java Chalnges/AddMultiNumbers.java
@@ -5,6 +5,7 @@ public class AddMultiNumbers {
        double result = nums(1,2,3,4,5,6,7,8);
         System.out.println(result);
     }
+    //clone satarting with 3 Dot (...)
     public static double nums(double ...nums){
         double sum = 0;
         for (double num : nums) {
