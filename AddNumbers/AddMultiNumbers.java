bbc182aea82e5ea4ae7e7d5ffbf305321e40e150
Blooming0 <154713499+Blooming0@users.noreply.github.com>
2024-11-02 12:05:45 +0300

    Java Chalnge
    using java clone method (function) in Adding multiple numbers 😁.

---
+++ AddNumbers/AddMultiNumbers.java
@@ -0,0 +1,15 @@
+package AddNumbers;
+
+public class AddMultiNumbers {
+    public static void main(String[] args) {
+       double result = nums(1,2,3,4,5,6,7,8);
+        System.out.println(result);
+    }
+    public static double nums(double ...nums){
+        double sum = 0;
+        for (double num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+}
